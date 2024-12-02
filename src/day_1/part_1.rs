e80f420a0f3bbe457bd17bd5fc20057624683b7c
Jack Sanders <jacklewissanders@gmail.com>
2024-12-01 20:30:31 -0600

    day_1

---
+++ src/day_1/part_1.rs
@@ -0,0 +1,43 @@
+use crate::utils::read_lines;
+
+pub fn main(file: &str) -> i32 {
+    let mut diff_sum: i32 = 0;
+
+    if let Ok(lines) = read_lines(file) {
+        let mut v1 = Vec::new();
+        let mut v2 = Vec::new();
+
+        for line in lines.flatten() {
+            let first = line.split_whitespace().next();
+            let last = line.split_whitespace().clone().last();
+            if first.is_some() {
+                v1.push(first.unwrap().parse::<i32>().unwrap());
+            }
+            if last.is_some() {
+                v2.push(last.unwrap().parse::<i32>().unwrap());
+            }
+        }
+
+        v1.sort();
+        v2.sort();
+
+        for i in 0..v1.len() {
+            diff_sum += (v1[i] - v2[1]).abs();
+        }
+    }
+    diff_sum
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day_1::part_1::main;
+
+    #[test]
+    fn test_sample_file() {
+        assert_eq!(main("src/day_1/files/test_1.txt"), 11);
+    }
+    #[test]
+    fn test_real_file() {
+        assert_eq!(main("src/day_1/files/input.txt"), 1889772);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod day_1;
+mod utils;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        main();
+    }
+}
