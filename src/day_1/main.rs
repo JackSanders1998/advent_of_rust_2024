2c2de779f01d4f335c7e7f1dfbea7086c170b86d
Jack Sanders <jack.sanders@tempus.com>
2024-12-02 13:59:41 -0600

    cleanup

--- src/day_1/main.rs
+++ src/day_1/main.rs
@@ -1,12 +1,10 @@
 use crate::utils::read_lines;
 
-pub fn part_1(file: &str) -> i32 {
-    let mut diff_sum: i32 = 0;
+fn helper(file: &str) -> (Vec<i32>, Vec<i32>) {
+    let mut v1 = Vec::new();
+    let mut v2 = Vec::new();
 
     if let Ok(lines) = read_lines(file) {
-        let mut v1 = Vec::new();
-        let mut v2 = Vec::new();
-
         for line in lines.flatten() {
             let first = line.split_whitespace().next();
             let last = line.split_whitespace().clone().last();
@@ -17,39 +15,33 @@ pub fn part_1(file: &str) -> i32 {
                 v2.push(last.unwrap().parse::<i32>().unwrap());
             }
         }
-
-        v1.sort();
-        v2.sort();
-
-        for i in 0..v1.len() {
-            diff_sum += (v1[i] - v2[i]).abs();
-        }
     }
+    (v1, v2)
+}
+
+pub fn part_1(file: &str) -> i32 {
+    let mut diff_sum: i32 = 0;
+
+    let (mut v1, mut v2) = helper(file);
+    v1.sort();
+    v2.sort();
+
+    for i in 0..v1.len() {
+        diff_sum += (v1[i] - v2[i]).abs();
+    }
+
     diff_sum
 }
 
 pub fn part_2(file: &str) -> i32 {
     let mut diff_sum: i32 = 0;
 
-    if let Ok(lines) = read_lines(file) {
-        let mut v1 = Vec::new();
-        let mut v2 = Vec::new();
+    let (v1, v2) = helper(file);
 
-        for line in lines.flatten() {
-            let first = line.split_whitespace().next();
-            let last = line.split_whitespace().clone().last();
-            if first.is_some() {
-                v1.push(first.unwrap().parse::<i32>().unwrap());
-            }
-            if last.is_some() {
-                v2.push(last.unwrap().parse::<i32>().unwrap());
-            }
-        }
-
-        for i in 0..v1.len() {
-            diff_sum += v1[i] * v2.iter().filter(|&n| *n == v1[i.clone()]).count() as i32;
-        }
+    for i in 0..v1.len() {
+        diff_sum += v1[i] * v2.iter().filter(|&n| *n == v1[i.clone()]).count() as i32;
     }
+
     diff_sum
 }
 
@@ -58,19 +50,19 @@ mod tests {
     use crate::day_1::main::{part_1, part_2};
 
     #[test]
-    fn test_sample_file() {
+    fn part_1_test_file() {
         assert_eq!(part_1("src/day_1/files/test.txt"), 11);
     }
     #[test]
-    fn test_real_file() {
+    fn part_1_input_file() {
         assert_eq!(part_1("src/day_1/files/input.txt"), 1889772);
     }
     #[test]
-    fn test_sample_file_2() {
+    fn part_2_test_file() {
         assert_eq!(part_2("src/day_1/files/test.txt"), 31);
     }
     #[test]
-    fn test_real_file_2() {
+    fn part_2_input_file() {
         assert_eq!(part_2("src/day_1/files/input.txt"), 23228917);
     }
 }
