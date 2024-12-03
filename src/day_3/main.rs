3d6158265575664fa1c535cd70499016be674003
Jack Sanders <jack.sanders@tempus.com>
2024-12-03 14:24:09 -0600

    push up

--- src/day_3/main.rs
+++ src/day_3/main.rs
@@ -18,17 +18,35 @@ fn find_multipliers(text: String) -> Vec<Vec<i32>> {
 
 pub fn part_1(file: &str) -> i32 {
     let mut total = 0;
+
     for line in read_lines(file).flatten() {
         for nums_to_multiply in find_multipliers(line) {
             total += nums_to_multiply[0] * nums_to_multiply[1];
         }
     }
+
     total
 }
 
 pub fn part_2(file: &str) -> i32 {
-    println!("Part 2 not implemented yet, {}", file);
-    0
+    let mut total = 0;
+
+    for line in read_lines(file).flatten() {
+        let mut vec: Vec<&str> = vec!();
+        line.split("do()").for_each(|s| {
+            let splits: Vec<&str> = s.split("don't()").collect();
+            vec.push(splits[0]);
+        });
+
+        for elem in vec {
+            println!("{:?}", elem);
+            for nums_to_multiply in find_multipliers(elem.to_string()) {
+                total += nums_to_multiply[0] * nums_to_multiply[1];
+            }
+        }
+        // break;
+    }
+    total
 }
 
 #[cfg(test)]
@@ -37,7 +55,7 @@ mod tests {
 
     #[test]
     fn part_1_test_file() {
-        assert_eq!(part_1("src/day_3/files/test.txt"), 161);
+        assert_eq!(part_1("src/day_3/files/test_1.txt"), 161);
     }
     #[test]
     fn part_1_input_file() {
@@ -45,10 +63,13 @@ mod tests {
     }
     #[test]
     fn part_2_test_file() {
-        assert_eq!(part_2("src/day_3/files/test.txt"), 0);
+        assert_eq!(part_2("src/day_3/files/test_2.txt"), 48);
     }
     #[test]
     fn part_2_input_file() {
         assert_eq!(part_2("src/day_3/files/input.txt"), 0);
+        // 108609098 is too high
+        // 17580588 just for the first line
+        // 184122457 is the first answer
     }
 }
